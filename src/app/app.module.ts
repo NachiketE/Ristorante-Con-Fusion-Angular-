5e89133948aa9fb02f30fe1b6caeb6343132f82d
NachiketE <nachiketerlekar@gmail.com>
2019-02-03 04:58:58 +0530

    Promise Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
@@ -65,6 +66,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     MatDialogModule,
     FormsModule,
+    MatProgressSpinnerModule,
     MatSelectModule,
     MatSlideToggleModule,
     ReactiveFormsModule
