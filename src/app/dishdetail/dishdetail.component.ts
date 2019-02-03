5c4802863f6718522fccee1dc66c760f02606c18
NachiketE <nachiketerlekar@gmail.com>
2019-02-03 18:08:33 +0530

    Reactive Forms Part 3

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -21,7 +21,10 @@ export class DishdetailComponent implements OnInit {
   constructor(private dishService: DishService, private route: ActivatedRoute, private  location:Location) { }
 
   ngOnInit() {
-    this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+
+    this.dishService.getDishIds()
+    .subscribe(dishIds => this.dishIds = dishIds);
+
     this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
     .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
@@ -31,6 +34,7 @@ export class DishdetailComponent implements OnInit {
     this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
     this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
+
   goBack(): void{
   this.location.back();
   }
